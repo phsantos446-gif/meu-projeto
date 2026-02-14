3bbc59f6c81a75dcac5ad0f52c9614ca4781da3c
phsantos446-gif <phsantos446@gmail.com>
2026-02-14 15:12:07 -0300

    primeiro commit

---
+++ e2e/login.spec.cy.js
@@ -0,0 +1,38 @@
+const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']"
+  }
+
+  const userData = {
+    userSucess: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      username: 'teste',
+      password: 'teste'
+    }
+  }
+
+  it('login - Sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+  })
+    it('login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
+    cy.get(selectorsList.loginButton).click()
+   // cy.get(selectorsList.wrongCredentialAlert)
+   })
+  
+  
+
